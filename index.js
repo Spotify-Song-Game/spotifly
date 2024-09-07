91ac2c02357a976774b5988a8fc80813bbeda1c1
Daniel Ng <ng17daniel@gmail.com>
2024-09-07 03:12:47 -0400

    bouncy and multiple cubes, will do controls next week

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//CONSTANTS
+const NUM_BARS = 30;
+const bar_width = 810/NUM_BARS;
+const MIN_Y = 600;
+const MAX_SPEED = 5;
+const ORIGIN = {x: 400, y: 200}
+
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -5,29 +12,41 @@ var Engine = Matter.Engine,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
 
-// create an engine
 var engine = Engine.create();
-
-// create a renderer
 var render = Render.create({
     element: document.body,
     engine: engine
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(0, 0, 80, 80);
-var boxB = Bodies.rectangle(400, 200, 80, 80);
 
-var bar_width = 81;
+var playerBox = Bodies.rectangle(400, 200, 55, 55);
+var enemyBoxes = []
+for(var i=0; i<5; i++){
+    var tempBox = Bodies.rectangle(i*200+10, 400, 55, 55)
+    tempBox.restitution = 1.5
+    enemyBoxes.push(tempBox);
+}
+engine.gravity = {x: 0, y: 1, scale: 0.0005}
+playerBox.restitution = 1.5;
+
+
+
 var ground = [];
-var barHeights = Array(10).fill(500);
-for (let i = 0; i < 10; i++) {
+var barHeights = Array(NUM_BARS).fill(400);
+for (let i = 0; i < NUM_BARS; i++) {
     ground.push(Bodies.rectangle(bar_width/2 + bar_width * i, 610, bar_width, barHeights[i], { isStatic: true }));
 }
 
+var walls = [];
+walls.push(Bodies.rectangle(-80,200, 80, 800, {isStatic: true}))
+walls.push(Bodies.rectangle(840,200, 80, 800, {isStatic: true}))
+
+
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB]);
+Composite.add(engine.world, playerBox);
 Composite.add(engine.world, ground);
+Composite.add(engine.world, walls);
+Composite.add(engine.world, enemyBoxes);
 
 
 // run the renderer
@@ -40,7 +59,7 @@ var runner = Runner.create();
 var audio = new Audio(); 
 
 audio.id = "audio_player";
-audio.src = "mp3/test.mp3";
+audio.src = "mp3/505.mp3";
 audio.controls = true; // not going to exist in the final, but for now it provides a button to start the music which starts the bars
 audio.loop = false; // loop or don't loop
 audio.autoplay = false; // maybe true later down the line? Automatically starts the music. We probably link the start of the music to a start button though.
@@ -66,28 +85,45 @@ window.addEventListener( // This is pretty useless at the end for our project, b
 );
 
 function myFunction(){
+    
+    //requests function call on next frame
     window.RequestAnimationFrame =
         window.requestAnimationFrame(myFunction) ||
         window.msRequestAnimationFrame(myFunction) ||
         window.mozRequestAnimationFrame(myFunction) ||
         window.webkitRequestAnimationFrame(myFunction);
 
+    
     fbc_array = new Uint8Array(analyser.frequencyBinCount); // Read about it yourself https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/frequencyBinCount
 
     analyser.getByteFrequencyData(fbc_array); // This is the only thing I don't yet understand, but it's the most important part
+    //https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getByteFrequencyData
 
     Composite.remove(engine.world, ground);
-    for (let i = 0; i < 10; i++) {
+
+    for (let i = 0; i < NUM_BARS; i++) {
         barHeights[i] = -(fbc_array[i] / 2);
-        if (barHeights[i] > -3){
-            barHeights[i] = -3
+        if (barHeights[i] > -10){
+            barHeights[i] = -10
         }
-        ground[i]=Bodies.rectangle(bar_width/2 + bar_width*i, 610, bar_width, barHeights[i]*10, {isStatic: true});
+        ground[i]=Bodies.rectangle(bar_width/2 + bar_width*i, 610, bar_width, barHeights[i]*4, {isStatic: true});
     }
 
         //alert("dawdj");
     Composite.add(engine.world, ground);
 
+    //player constraints
+
+    /*
+    if(playerBox.position.y > MIN_Y){ //this is broken idk why
+        Matter.Body.setPosition(ORIGIN);
+    }
+    
+    if(playerBox.speed > 3){ //idk if this works
+        var unitVector = playerBox.velocity / playerBox.speed;
+        Matter.Body.setVelocity(playerBox, unitVector * 3);
+    }
+    */
 }
 
 // run the engine
