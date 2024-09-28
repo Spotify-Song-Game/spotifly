420ae6a0eeb63cf5173be54748c2d4b605047989
Daniel Ng <ng17daniel@gmail.com>
2024-09-27 23:03:39 -0400

    added some comments to clarify added options for engine width, height, wireframe

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ const bar_width = 810/NUM_BARS; // width of each bar
 const MIN_Y = 600; //minimum y level before game resets cube (BROKEN)
 const MAX_SPEED = 5; //speed cap (BROKEN)
 const ORIGIN = {x: 400, y: 200} //where to teleport cube when out of bounds (BROKEN)
+const SCREEN_WIDTH = 1000;
+const SCREEN_HEIGHT = 800;
 
-// module aliases
+// makes life easier so we don't have to call from matter class all the time
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
+//
 
-var engine = Engine.create();
-var render = Render.create({
-    element: document.body,
-    engine: engine
+var engine = Engine.create(); //instantiates the engine object which runs the physics engine
+
+var render = Render.create({ //instantiates a render object which actually displays the page
+    element: document.body, //sets the element attribute to render within the document body
+    engine: engine, //sets the engine attribute of render to the recently instantiated engine object above
+    options: {
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
+        wireframes: false
+    }
 });
 
-
-var playerBox = Bodies.rectangle(400, 200, 55, 55);
-var enemyBoxes = []
+var playerBox = Bodies.rectangle(400, 200, 55, 55); //creating a square that should represent the player
+var enemyBoxes = [] //empty array for adding enemy boxes
 for(var i=0; i<5; i++){
-    var tempBox = Bodies.rectangle(i*200+10, 400, 55, 55)
-    tempBox.restitution = 1.5
-    enemyBoxes.push(tempBox);
+    var enemyBox = Bodies.rectangle(i*200+10, 400, 55, 55)
+    enemyBox.restitution = 1.5//sets bounce/elasticity to more than 1 so it bounces a lot
+    enemyBoxes.push(enemyBox); //adds enemyBox to enemyboxes array 
 }
 engine.gravity = {x: 0, y: 1, scale: 0.0005}
 playerBox.restitution = 1.5;
-
-
 
 var ground = [];
 var barHeights = Array(NUM_BARS).fill(400);
@@ -41,7 +47,6 @@ var walls = [];
 walls.push(Bodies.rectangle(-80,200, 80, 10000, {isStatic: true}))
 walls.push(Bodies.rectangle(840,200, 80, 10000, {isStatic: true}))
 
-
 // add all of the bodies to the world
 Composite.add(engine.world, playerBox);
 Composite.add(engine.world, ground);
@@ -49,14 +54,11 @@ Composite.add(engine.world, walls);
 //Composite.add(engine.world, Bodies.rectangle(300, -10, 1000, 10, {isStatic: true}))
 Composite.add(engine.world, enemyBoxes);
 
-
 // run the renderer
 Render.run(render);
 
 // create runner
 var runner = Runner.create();
-
-
 var audio = new Audio(); 
 
 audio.id = "audio_player";
